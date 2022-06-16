1d1c06c4a8a9cd19a99193108f29d8a724be0321
Kal1Vane <82412065+Kal1Vane@users.noreply.github.com>
2022-06-16 16:17:07 +0500

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -23,13 +23,13 @@ export enum AuthorizationStatus {
 }
 
 export const defaultCustomIcon = leaflet.icon({
-  iconUrl: 'img/pin.svg',
+  iconUrl: '/img/pin.svg',
   iconSize: [27, 39],
   iconAnchor: [13.5, 39],
 });
 
 export const currentCustomIcon = leaflet.icon({
-  iconUrl: 'img/pin-active.svg',
+  iconUrl: '/img/pin-active.svg',
   iconSize: [27, 39],
   iconAnchor: [13.5, 39],
 });
